dee7176767bbcfe14e6529fd5091e377efce8655
dulcefabiola <62074962+DulceFabiola@users.noreply.github.com>
2022-01-31 15:04:07 -0600

    estilos

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -34,105 +34,111 @@ const Checkout = () => {
   return (
     <div className="card-body">
       <ConsultDetails />
-      <h1>Agregar servicios extras a la consulta</h1>
-      <div>
-        <div className="separator">
-          <p className="title">Estudios:</p>
-          <table className="services">
-            <tr>
-              <th>Descipción</th>
-              <th>Precio</th>
-              <th>Agregar</th>
-            </tr>
-            {services.map((item) => {
-              return (
-                <>
-                  {item.type === "Study" && (
-                    <tr key={item._id}>
-                      <td>{item.description}</td>
-                      <td>{item.price}</td>
-                      <td>
-                        <input
-                          type={"checkbox"}
-                          checked={checkedItems[item.description]}
-                          onChange={handleChange}
-                          name={item._id}
-                          value={item._id}
-                        />
-                      </td>
-                    </tr>
-                  )}
-                </>
-              );
-            })}
-          </table>
-        </div>
-        <div className="separator">
-          <p className="title">Servicios:</p>
-          <table className="services">
-            <tr>
-              <th>Descipción</th>
-              <th>Precio</th>
-              <th>Agregar</th>
-            </tr>
-            {services.map((item) => {
-              return (
-                <>
-                  {item.type === "Service" && (
-                    <tr key={item._id}>
-                      <td>{item.description}</td>
-                      <td>{item.price}</td>
-                      <td>
-                        <input
-                          type={"checkbox"}
-                          checked={checkedItems[item.description]}
-                          onChange={handleChange}
-                          name={item._id}
-                          value={item._id}
-                        />
-                      </td>
-                    </tr>
-                  )}
-                </>
-              );
-            })}
-          </table>
-        </div>
-        <div className="separator">
-          <p className="title">Productos:</p>
-          <table className="services">
-            <tr>
-              <th>Descipción</th>
-              <th>Precio</th>
-              <th>Agregar</th>
-            </tr>
-            {services.map((item) => {
-              return (
-                <>
-                  {item.type === "Product" && (
-                    <tr key={item._id}>
-                      <td>{item.description}</td>
-                      <td>{item.price}</td>
-                      <td>
-                        <input
-                          type={"checkbox"}
-                          checked={checkedItems[item.description]}
-                          onChange={handleChange}
-                          name={item._id}
-                          value={item._id}
-                        />
-                      </td>
-                    </tr>
-                  )}
-                </>
-              );
-            })}
-          </table>
-          <div className="div-right">
-            <h3>Total:</h3>
-            <button type="submit" className="btn">
-              Finalizar consulta
-            </button>
+      <div className="patients-container">
+        <div className="card">
+          <div className="card-body">
+            <h1>Agregar servicios extras a la consulta</h1>
+            <div>
+              <div className="separator">
+                <p className="title">Estudios:</p>
+                <table className="services">
+                  <tr>
+                    <th>Descipción</th>
+                    <th>Precio</th>
+                    <th>Agregar</th>
+                  </tr>
+                  {services.map((item) => {
+                    return (
+                      <>
+                        {item.type === "Study" && (
+                          <tr key={item._id}>
+                            <td>{item.description}</td>
+                            <td>{item.price}</td>
+                            <td>
+                              <input
+                                type={"checkbox"}
+                                checked={checkedItems[item.description]}
+                                onChange={handleChange}
+                                name={item._id}
+                                value={item._id}
+                              />
+                            </td>
+                          </tr>
+                        )}
+                      </>
+                    );
+                  })}
+                </table>
+              </div>
+              <div className="separator">
+                <p className="title">Servicios:</p>
+                <table className="services">
+                  <tr>
+                    <th>Descipción</th>
+                    <th>Precio</th>
+                    <th>Agregar</th>
+                  </tr>
+                  {services.map((item) => {
+                    return (
+                      <>
+                        {item.type === "Service" && (
+                          <tr key={item._id}>
+                            <td>{item.description}</td>
+                            <td>{item.price}</td>
+                            <td>
+                              <input
+                                type={"checkbox"}
+                                checked={checkedItems[item.description]}
+                                onChange={handleChange}
+                                name={item._id}
+                                value={item._id}
+                              />
+                            </td>
+                          </tr>
+                        )}
+                      </>
+                    );
+                  })}
+                </table>
+              </div>
+              <div className="separator">
+                <p className="title">Productos:</p>
+                <table className="services">
+                  <tr>
+                    <th>Descipción</th>
+                    <th>Precio</th>
+                    <th>Agregar</th>
+                  </tr>
+                  {services.map((item) => {
+                    return (
+                      <>
+                        {item.type === "Product" && (
+                          <tr key={item._id}>
+                            <td>{item.description}</td>
+                            <td>{item.price}</td>
+                            <td>
+                              <input
+                                type={"checkbox"}
+                                checked={checkedItems[item.description]}
+                                onChange={handleChange}
+                                name={item._id}
+                                value={item._id}
+                              />
+                            </td>
+                          </tr>
+                        )}
+                      </>
+                    );
+                  })}
+                </table>
+                <div className="div-right">
+                  <h3>Total:</h3>
+                  <button type="submit" className="btn">
+                    Finalizar consulta
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
